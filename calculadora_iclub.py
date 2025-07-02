c13b488b262e78c14ce7b2cffa38132ba51f03c9
iclubstoree <iclubstoree@gmail.com>
2025-07-02 18:24:41 -0300

    Update calculadora_iclub.py

--- calculadora_iclub.py
+++ calculadora_iclub.py
@@ -102,12 +102,15 @@ if valor_total is not None:
         entrada_formatada = f"R$ {(valor_entrada or 0):,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
         if valor_entrada > 0:
-            texto_copia = f"{entrada_formatada} + {parcela_formatada}" if parcelas == 0 else f"{entrada_formatada} + {parcelas}x {parcela_formatada}"
-        else:
-            texto_copia = f"{parcela_formatada}" if parcelas == 0 else f"{parcelas}x {parcela_formatada}"f"{parcela_formatada}" if parcelas == 0 else f"{parcelas}x {parcela_formatada}"
+            if parcelas == 0:
+                texto_copia = f"{entrada_formatada} + {parcela_formatada}"
+            else:
                 texto_copia = f"{entrada_formatada} + {parcelas}x {parcela_formatada}"
         else:
-            texto_copia = f"{parcela_formatada}" if parcelas == 0 else f"{parcelas}x {parcela_formatada}"
+            if parcelas == 0:
+                texto_copia = f"{parcela_formatada}"
+            else:
+                texto_copia = f"{parcelas}x {parcela_formatada}"f"{parcela_formatada}" if parcelas == 0 else f"{parcelas}x {parcela_formatada}"
 
         linha_html = f'''
         <div class='row'>
